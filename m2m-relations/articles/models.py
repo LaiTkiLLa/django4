6050413359f1c033747e04f5654467fb7a980f8d
LaiTkiLLa <84670285+LaiTkiLLa@users.noreply.github.com>
2022-07-28 21:02:23 +0300

    Update models.py

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-
 class Scope(models.Model):
-
-    name = models.CharField(max_length=256, verbose_name='Раздел')
+    name = models.CharField(max_length=30)
 
     class Meta:
         verbose_name = 'Раздел'
@@ -12,13 +10,14 @@ class Scope(models.Model):
     def __str__(self):
         return self.name
 
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
-    scope = models.ManyToManyField(Scope, through='Scope_Aricle')
+    scope = models.ManyToManyField(Scope, through='Scope_Aricle', related_name='tags')
 
     class Meta:
         verbose_name = 'Статья'
@@ -27,12 +26,11 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
-
-
 class Scope_Aricle(models.Model):
+    tag = models.ForeignKey(Scope, on_delete=models.CASCADE,verbose_name='Раздел')
+    is_main = models.BooleanField(default=False,verbose_name='Основной')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
 
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scope_article')
-    scope = models.ForeignKey(Scope, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.article}_{self.scope}'
+    class Meta:
+        verbose_name = 'раздел для статьи'
+        verbose_name_plural = 'Тематика статьи'
