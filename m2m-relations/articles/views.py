6a15c4138f3c5ffce2dec50913e4595df7f6646d
LaiTkiLLa <84670285+LaiTkiLLa@users.noreply.github.com>
2022-07-28 21:01:30 +0300

    Update views.py

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 
-from articles.models import Article, Scope_Aricle
-
+from articles.models import Article,Scope_Aricle
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {'object_list': Article.objects.all(),' article.scopes.all': Scope_Aricle.objects.all()}
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    context = {'object_list': Article.objects.order_by('-published_at').all(),
+             }
 
     return render(request, template, context)
+
